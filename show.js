396f440c6212d4875b5eebe2dbe33f3e09c57e3f
colconstruccion <rvelezpantoja@gmail.com>
2025-09-15 16:00:50 -0400

    Add files via upload
    let ocupada = revisarPosicion(coor_x,coor_y); added to iniciarVisitantes() in geomegol_v40.js 
    random song for gol added to show.js

--- show.js
+++ show.js
@@ -1,5 +1,5 @@
-const golesAudio =  ['golA1.mp3','golA2.mp3','golRincon.wav','golTren.mp3','golTino.mp3'];
-var golAudio = new Audio("audio/"+golesAudio[marcador1.value]); 
+const golesAudio =  ['golA1.mp3','golA2.mp3','golRincon.wav','golTren.mp3','golTino.mp3','golCanteme.mp3'];
+
 
 function coloresTablero() {
  return "#"+Math.random().toString(16).slice(-6);
@@ -19,6 +19,8 @@ function flashColors(seconds){
     }, seconds * 1000);
 
     //Cantar el gol
+    let cancion = Math.floor(Math.random()*7);
+    let golAudio = new Audio("audio/"+golesAudio[cancion]); 
     golAudio.play();
     //let duration = golRincon.duration;
     //console.log(duration);
